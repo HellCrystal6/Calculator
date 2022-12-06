272a6600445e9cadc88ecc98088d1b423f801cad
HellCrystal6 <94065325+HellCrystal6@users.noreply.github.com>
2022-12-06 13:12:36 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,69 @@
+print(''' ________          ________          ___               ________          ___  ___          ___               ________          _________        ________          ________     
+|\   ____\        |\   __  \        |\  \             |\   ____\        |\  \|\  \        |\  \             |\   __  \        |\___   ___\     |\   __  \        |\   __  \    
+\ \  \___|        \ \  \|\  \       \ \  \            \ \  \___|        \ \  \\\  \       \ \  \            \ \  \|\  \       \|___ \  \_|     \ \  \|\  \       \ \  \|\  \   
+ \ \  \            \ \   __  \       \ \  \            \ \  \            \ \  \\\  \       \ \  \            \ \   __  \           \ \  \       \ \  \\\  \       \ \   _  _\  
+  \ \  \____        \ \  \ \  \       \ \  \____        \ \  \____        \ \  \\\  \       \ \  \____        \ \  \ \  \           \ \  \       \ \  \\\  \       \ \  \\  \| 
+   \ \_______\       \ \__\ \__\       \ \_______\       \ \_______\       \ \_______\       \ \_______\       \ \__\ \__\           \ \__\       \ \_______\       \ \__\\ _\ 
+    \|_______|        \|__|\|__|        \|_______|        \|_______|        \|_______|        \|_______|        \|__|\|__|            \|__|        \|_______|        \|__|\|__|''')
+
+
+def expressions():
+    try:
+        while True:
+            num = input("Enter an expression: ")
+            print("The answer is: ", eval(num))
+    except KeyboardInterrupt:
+        print('\n')
+        print("Exiting...")
+
+    
+def calc():
+    print('Press Ctrl-C to quit.')
+    try:
+        while True:
+            firstnum = int(input("What is your first number?: "))
+            secondnum = int(input("What is your second number?: "))
+            symbol = input("Would you like to multiply or divide or add or subtract?: ")
+            multiply = "*"
+            divide = "/"
+            add = "+"
+            subtract = "-"
+
+            if symbol == multiply:
+                print("The answer is: " + str(firstnum * secondnum))
+
+            if symbol == divide:
+                print("The answer is: " + str(firstnum / secondnum))
+    
+            if symbol == add:
+                print("The answer is: " + str(firstnum + secondnum))
+    
+            if symbol == subtract:
+                print("The answer is: " + str(firstnum - secondnum))
+
+    except KeyboardInterrupt:
+        print('\n')
+        print("Exiting...")
+
+print("What type of calculator would you like to use?")
+calctype = input('''1. Expressions
+2. Normal (type Expressions or Normal): ''')
+
+if calctype == "Normal":
+    calc()
+
+if calctype == "Expressions":
+    expressions()
+    
+if calctype == "normal":
+    calc()
+
+if calctype == "expressions":
+    expressions()
+########################################################
+# Enter an expression: 10*20 or 1880+2076 or 1000-55050
+########################################################
+# Enter a string: Nonu is coding!
+# Enter position: 3
+# Character at 3 is: u
+########################################################
